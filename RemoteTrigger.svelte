bd3cff5d5b13e235e974258d89a0f7ed93c0b663
VyankteshKoli <141150425+VyankteshKoli@users.noreply.github.com>
2024-07-24 10:32:15 +0530

    Update RemoteTrigger.svelte

--- RemoteTrigger.svelte
+++ RemoteTrigger.svelte
@@ -1,11 +1,11 @@
 <script>
-  import { toggleSpace } from '../modules/tv.js';
+  import { toggleSpace } from 'tv.js';
 </script>
 
 <button class="remote-trigger cursor-pointer" on:click={() => toggleSpace()}>
   <img
     loading="lazy"
-    src="/assets/images/remote-control.svg"
+    src="remote-control.svg"
     alt="remote"
     width="32"
     height="32" />
